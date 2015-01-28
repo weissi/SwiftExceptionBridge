1da179375c4d062c2060e19dc6f4f19a32f1a4d4
Johannes Weiß <johannes.weiss@bromium.com>
2015-01-28 15:08:31 +0000

    Initial Commit

---
+++ SwiftExceptionBridge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftExceptionBridge
+//
+//  Created by Johannes Weiß on 28/01/2015.
+//  Copyright (c) 2015 Johannes Weiß. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
